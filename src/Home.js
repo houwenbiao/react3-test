9270a327940c27e13e0aa2021d709c0b3767cd44
JackHou <jackhou1990@163.com>
2019-07-11 18:22:22 +0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import {observer} from 'mobx-react'
+
+
+export default @observer
+class Home extends Component {
+    render() {
+        return <div>
+            Hello, jack
+        </div>
+    }
+}
